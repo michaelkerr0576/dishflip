514308aa32e0b19ba7548ae4b578e1d6969051e8
michaelkerr0576 <53580213+michaelkerr0576@users.noreply.github.com>
2020-08-26 00:24:05 +0100

    heroku fix 4

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
+const path = require("path");
 const express = require("express");
-const http = require("http");
-const app = require("./app");
+const app = express();
+const publicPath = path.join(__dirname, "..", "dishflip/client/build");
+const port = process.env.PORT || 3000;
 
-// Static folder
-app.use(express.static(__dirname + "//../client/build"));
-// Handle single page app
-app.get(/.*/, (req, res) =>
-  res.sendFile(__dirname + "//../client/build/index.html")
-);
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
-const port = process.env.PORT || 5000;
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
